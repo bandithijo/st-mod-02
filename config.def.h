ccf1265e1144114fc339c753c5878ddce6a22f01
Rizqi Nur Assyaufi <bandithijo@gmail.com>
2024-05-08 01:32:44 +0800

    patch: font2

--- config.def.h
+++ config.def.h
@@ -5,7 +5,12 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "Liberation Mono:pixelsize=12:antialias=true:autohint=true";
+static char *font = "JetBrainsMono Nerd Font Bandit:style=Medium:pixelsize=15:antialias=true:autohint=true";
+/* Spare fonts */
+static char *font2[] = {
+	"Twitter Color Emoji:pixelsize=15:antialias=true:autohint=true",
+};
+
 static int borderpx = 2;
 
 /*
